8105883dd598779eb08c3084343a1f5a948312e4
hmh <hasanmo3080vr@gmail.com>
2026-08-25 00:45:21 +0300

    chore: release 0.4.2

--- src/altair/core/version.cr
+++ src/altair/core/version.cr
@@ -3,5 +3,5 @@
 # This file defines the framework version constant. Every component of Altair
 # reports against this single source of truth.
 module Altair
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 end
